276b232cec5188eba3857567ab5195ac6e64b8f8
Fatih <fatih.mutlu@epfl.ch>
2020-12-05 23:07:18 +0100

    Merged

--- build_vocab.sh
+++ build_vocab.sh
@@ -3,4 +3,6 @@
 # Note that this script uses GNU-style sed as gsed. On Mac OS, you are required to first https://brew.sh/
 #    brew install gnu-sed
 # on linux, use sed instead of gsed in the command below:
-cat twitter-datasets/train_pos_full.txt twitter-datasets/train_neg_full.txt | gsed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab_full.txt
+
+# Outputs all the (unique) words sorted along with their number of occurrences 
+cat data/train_pos_full.txt data/train_neg_full.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab_full.txt
--- cut_vocab.sh
+++ cut_vocab.sh
@@ -3,4 +3,6 @@
 # Note that this script uses GNU-style sed as gsed. On Mac OS, you are required to first https://brew.sh/
 #    brew install gnu-sed
 # on linux, use sed instead of gsed in the command below:
-cat vocab_full.txt | gsed "s/^\s\+//g" | sort -rn | grep -v "^[1234]\s" | cut -d' ' -f2 > vocab_cut.txt
+
+# Outputs the list of words occurring at least 5 times, by the inverse order of the number of occurrences
+cat vocab_full.txt | sed "s/^\s\+//g" | sort -rn | grep -v "^[1234]\s" | cut -d' ' -f2 > vocab_cut.txt
