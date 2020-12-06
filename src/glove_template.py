c18718b3b8ae4e64004ed47838e5735084b9a8ee
Loïc BUSSON <loic.busson@epfl.ch>
2020-12-06 02:18:04 +0100

    GloVe implementation + description

--- src/glove_template.py
+++ src/glove_template.py
@@ -1,3 +1,20 @@
+'''
+File name: glove_template.py
+Author: Fatih Mutlu, Loïc Busson, Nicolas Thierry d’Argenlieu
+Date created: 23/11/2020
+Date last modified: 06/12/2020
+Python Version: 3.8.5
+
+glove_template.py is a python file that takes a previously computed co-occurence matrix
+and returns the matrix xs containing the word vectors computed with GloVe. 
+
+GloVe uses the following loss function: J = \sum_{i,j}^{V} f(M_{ij})(w_i^Tw_j + b_i + c_j - log(M_{ij}))^2
+For now, we took b_i = c_j = 0 --> room to improvement
+
+After considering the gradient respecting...
+to continue after the computations
+'''
+
 #!/usr/bin/env python3
 from scipy.sparse import *
 import numpy as np
@@ -27,10 +44,12 @@ def main():
     for epoch in range(epochs):
         print("epoch {}".format(epoch))
         for ix, jy, n in zip(cooc.row, cooc.col, cooc.data):
-
-			# fill in your SGD code here, 
-			# for the update resulting from co-occurence (i,j)
-		
+			logn = np.log(n)
+            fn = min(1.0, (n / nmax) ** alpha)
+            x, y = xs[ix, :], ys[jy, :]
+            scale = 2 * eta * fn * (logn - np.dot(x, y))
+            xs[ix, :] += scale * y
+            ys[jy, :] += scale * x
 
     np.save('embeddings', xs)
 
