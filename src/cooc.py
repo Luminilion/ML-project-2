cad2290e408d4a5581221d1dc479c46c72d3553e
Loïc BUSSON <loic.busson@epfl.ch>
2020-12-07 19:47:38 +0100

    Optimized it so that it works on the full dataset

--- src/cooc.py
+++ src/cooc.py
@@ -2,13 +2,10 @@
 from scipy.sparse import *
 import numpy as np
 import pickle
+from collections import Counter
 
 """
 Description
-
-FYI:
-train_pos.txt is the 100 000 first lines of train_pos_full.txt (which contrains 1 250 000 lines).
-Same for train_neg.txt.
 
 ###First Part: 
 Open the previously constructed vocab.pkl and retrieve the dictionary as vocab.
@@ -35,11 +32,11 @@ Will do the following for both train_pos_full.txt and train_neg_full.txt:
     and column corresponding to a word in the vocabulary (0-th column i the 0-th word is the one with id 0 in the dictionnary)
     The value at the index [i,j] correspond to the number of times i and j occured together.
 
-    So now, for every index t in tokens, we will go over all the index t2 in tokens and say that
-    t (row.append(t)) and t2 (col.append(t2)) co-occur once (data.append(1))
+    So now, for every index t in tokens, we will go over all the index t2 in tokens and increase the count of (t,t2) in the Counter
+    by 1.
 
 When all of this is done, we will get three lists:
-- data : filled with 1's --> the entries of the matrix
+- data : filled with integers --> the entries of the matrix
 - row : filled with ids --> the row indices of the matrix entries
 - col : filled with ids --> the column indicies of the matrix entries
 
@@ -50,43 +47,38 @@ Example:
 
 # Constructing a matrix with duplicate indices
 
-row  = np.array([0, 0, 1, 3, 1, 0, 0])
+row  = np.array([0, 0, 1, 3])
 
-col  = np.array([0, 2, 1, 3, 1, 0, 0])
+col  = np.array([0, 2, 1, 3])
 
-data = np.array([1, 1, 1, 1, 1, 1, 1])
+data = np.array([3, 1, 2, 1])
 
 coo = coo_matrix((data, (row, col)), shape=(4, 4))
 
-# Duplicate indices are maintained until implicitly or explicitly summed
-
-np.max(coo.data)
-1
-
-coo.toarray()
-array([[3, 0, 1, 0],
-       [0, 2, 0, 0],
-       [0, 0, 0, 0],
-       [0, 0, 0, 1]])
-
-So here, we see that 0 occurs with 2 once in the array (and we see it as well in row and col)
-
-However, as said in the exemple, duplicate indices are maintained until implicitly summed.
-
-This is why we add : cooc.sum_duplicates at the end.
 
 Now that we have our coo_matrix, we store it as a pkl in cooc.pkl.
 """
 
 DATA_PATH = "../data/"
 
+def get_lists(dictionary):
+    row = []
+    col = []
+
+    for elem in dictionary.keys():
+        row.append(elem[0])
+        col.append(elem[1])
+
+    return row,col
+
 def main():
     with open(DATA_PATH + 'vocab.pkl', 'rb') as f:
         vocab = pickle.load(f)
     vocab_size = len(vocab)
 
-    data, row, col = [], [], []
     counter = 1
+    dic = Counter()
+
     for fn in [DATA_PATH + 'train_pos_full.txt', DATA_PATH + 'train_neg_full.txt']:
         with open(fn) as f:
             for line in f:
@@ -94,18 +86,18 @@ def main():
                 tokens = [t for t in tokens if t >= 0]
                 for t in tokens:
                     for t2 in tokens:
-                        data.append(1)
-                        row.append(t)
-                        col.append(t2)
+                        dic[(t,t2)] += 1
 
                 if counter % 10000 == 0:
                     print(counter)
                 counter += 1
-    cooc = coo_matrix((data, (row, col)))
-    print("summing duplicates (this can take a while)")
-    cooc.sum_duplicates()
+
+    row, col = get_lists(dic)
+    cooc = coo_matrix((list(dic.values()), (row, col)))
+
     with open(DATA_PATH + 'cooc.pkl', 'wb') as f:
         pickle.dump(cooc, f, pickle.HIGHEST_PROTOCOL)
+        print("Coo_matrix created and stored under 'cooc.pkl' in the data folder")
 
 
 if __name__ == '__main__':
