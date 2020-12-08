24135ef8a74d7468f6057c27adebc059528e388b
Loïc BUSSON <loic.busson@epfl.ch>
2020-12-08 17:18:08 +0100

    function to plot loss_function

---
+++ src/glove_loss_plot.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+DATA_PATH = "../data/"
+
+loss = np.load(DATA_PATH + "loss_per_epoch.npy")
+
+epoch = [i for i in range(len(loss))]
+
+plt.plot(epoch,loss)
+plt.title("Loss Function through epochs")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.save('../plots/Loss_function_{}_epochs'.format(len(loss)))
+plt.show()
